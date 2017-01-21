1dcb3511ec38c287fb9976aba91f77da9ac34a4b
Sean Esopenko <sesopenko@gmail.com>
2017-01-21 12:24:11 -0700

    Get node.js 6.x LTS going

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-apt-get update
-
 # don't prompt for mysql root password.  use 'password'
 echo "mysql-server mysql-server/root_password password password" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password password" | sudo debconf-set-selections
 
-# nodejs npm dependencies & mysql server
-apt-get install -y mysql-server build-essential libssl-dev nodejs npm
+# add apt source for nodejs
+cp /vagrant_setup/nodesource.list /etc/apt/sources.list.d/nodesource.list
+cat /vagrant_setup/nodesource.gpg.key | apt-key add -
+
+apt-get update
+
+# install packages
+apt-get install -y mysql-server build-essential libssl-dev nodejs
 
 # open firewall
 ufw allow 80
